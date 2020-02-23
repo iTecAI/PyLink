95413801dbb4232b2c5ad74cf8c383f55c9266f6
iTecAI <matteovh@gmail.com>
2020-02-23 07:38:43 -0500

    handler functionality

--- __init__.py
+++ __init__.py
@@ -9,13 +9,26 @@ class PyLink:
             self.link = link
         def do_GET(self):
             command_json = json.loads(self.rfile.read())
-            code, data = self.link.commands[command_json['command']](command_json['kwargs'])
+            try:
+                code, data = self.link.commands[command_json['command']](command_json['kwargs'])
+                self.send_response(code)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin','*')
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps(data),'utf-8'))
+            except KeyError:
+                self.send_response(404)
+                self.end_headers()
 
     def __init__(self,ip,server_port,api_port,directory=None,python_path='python',**commands):
         self.server = None
         self.api = ThreadingHTTPServer((ip,api_port),BaseHTTPRequestHandler)
         self.commands = commands
+        self.python_path = python_path
+        self.ip = ip
+        self.server_port = server_port
+        self.directory = directory
     
     def serve_forever(self):
-        self.server = Popen([python_path,ip,server_port,str(directory)])
+        self.server = Popen([self.python_path,self.ip,self.server_port,str(self.directory)])
         self.api.serve_forever()
