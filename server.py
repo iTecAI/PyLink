8c51ecf383b7c8a7d0dab3ba59cbe28f3e346705
iTecX <30746447+iTecAI@users.noreply.github.com>
2020-02-28 07:31:57 -0500

    updoot

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import sys
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 
 ip = sys.argv[1]
-port = sys.argv[2]
+port = int(sys.argv[2])
 direc = sys.argv[3]
 
 server = ThreadingHTTPServer((ip,port),SimpleHTTPRequestHandler)
